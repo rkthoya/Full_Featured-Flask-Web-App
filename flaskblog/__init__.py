20265a23fe97908d7256fff6cf1c01d9427c6184
rkthoya <rkthoya@gmail.com>
2020-04-08 18:31:13 +0300

    User Authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -8,5 +9,9 @@ app.config['SECRET_KEY'] ='58846c0e4d2ef5e6761de743b47d3731095865fe047db2d50288d
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
